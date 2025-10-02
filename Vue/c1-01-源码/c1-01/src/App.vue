2f04e7957801203f86acbeb6bf6517574d09a0be
renjoji kureha <nimadezufen@gmail.com>
2025-10-02 23:32:16 +0800

    vue01

---
+++ Vue/c1-01-源码/c1-01/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+  data(){
+    return{
+      msg:'文本插值',
+      hi:'hello world'
+    }
+  }
+}
+
+</script>
+
+<template>
+  
+<h3> 模板语法 </h3>
+
+<p>{{ msg }}</p>
+
+<p>{{ hi }}</p>
+
+</template>
+
+<style scoped></style>
---
+++ Vue/note.md
@@ -0,0 +1,2 @@
+创建vue项目：npm init vue@latest
+然后：npm install | npm dev
