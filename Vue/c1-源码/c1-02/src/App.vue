da86e566ffe36a7f56a9a64f38cc63c6f0e6685d
renjoji kureha <nimadezufen@gmail.com>
2025-10-04 20:50:41 +0800

    vue03

--- Vue/c1-源码/c1-02/src/App.vue
+++ Vue/c1-源码/c1-02/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
+import ifDemo from './components/ifDemo.vue';
 </script>
 
 <template>
   <HelloWorld/>
+  <ifDemo/>
 </template>
 
 <style scoped></style>
---
+++ Vue/c1-源码/c1-02/src/components/ifDemo.vue
@@ -0,0 +1,30 @@
+<script>
+
+export default {
+    data(){
+        return{
+            flag:false, // true可见，false不可见
+            type:"D",
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <h3>条件渲染</h3>
+
+    <div v-if="flag">can u see me</div>
+    <div v-else>now you only see me</div>
+
+    <div v-if="type==='A'">A</div>
+    <div v-else-if="type==='B'">B</div>
+    <div v-else-if="type==='C'">C</div>
+    <div v-else>Not A/B/C</div>
+
+    <div v-show="flag">u also can see me</div>
+
+    <!-- 改变次数少用v-if，改变次数多用v-show -->
+
+</template>
