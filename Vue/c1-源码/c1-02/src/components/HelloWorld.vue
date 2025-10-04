229650acf36b34d6fbfc7f9c22d214a3a9bb2d4b
renjoji kureha <nimadezufen@gmail.com>
2025-10-04 17:46:11 +0800

    vue02

---
+++ Vue/c1-源码/c1-02/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script>
+
+export default{
+    data(){
+        return{
+            dynamicClass:"appclass",
+            dynamicId:"appID",
+            dynamicTitle:null,  // null和undefined会使属性被移出
+            inButtonDisabled:true,  // 为true不可点击,为false可点
+            objectOfAttr:{
+                class:"appclass",
+                id:"appID",  
+            }
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <div class="{{ dynamicClass }}">双大括号不能用于属性赋值</div>
+
+    <div v-bind:class="dynamicClass" v-bind:id="dynamicId" v-bind:title="dynamicTitle">要使用v-bind:属性</div>
+
+    <div :class="dynamicClass" :id="dynamicId" :title="dynamicTitle">也可以不加v-bind, 简写直接:属性</div>
+
+    <button :disabled="inButtonDisabled">这是一个按钮</button>
+
+    <div v-bind="objectOfAttr">可以一次性绑定多个属性</div>
+
+</template>
+
+<style>
+
+.appclass{
+    color: rebeccapurple;
+    font-size: 30px;
+}
+
+</style>
