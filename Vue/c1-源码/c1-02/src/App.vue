7e47b06dbe3b7ab941a25be997987a03a51a6c8e
renjoji kureha <nimadezufen@gmail.com>
2025-10-06 21:19:35 +0800

    vue08

--- Vue/c1-源码/c1-02/src/App.vue
+++ Vue/c1-源码/c1-02/src/App.vue
@@ -9,6 +9,8 @@ import eventDemo2 from './components/06-event2.vue';
 import eventDemo3 from './components/07-event3.vue';
 import eventDemo4 from './components/07-event4.vue';
 import eventDemo5 from './components/07-event5.vue';
+import eventDemo6 from './components/08-event6.vue';
+
 
 </script>
 
@@ -20,9 +22,11 @@ import eventDemo5 from './components/07-event5.vue';
   <!-- <keyDemo/> -->
   <!-- <eventDemo1/> -->
   <!-- <eventDemo2/> -->
-  <eventDemo3/>
-  <eventDemo4/>
-  <eventDemo5/>
+  <!-- <eventDemo3/> -->
+  <!-- <eventDemo4/> -->
+  <!-- <eventDemo5/> -->
+  <eventDemo6/>
+
 
 </template>
 
---
+++ Vue/c1-源码/c1-02/src/components/08-event6.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+    data(){
+        return{
+        }
+    },
+    methods:{
+        clickHandle(e){
+            // 阻止默认事件
+            // e.preventDefault();
+            console.log("点击了");
+        },
+
+        clickDiv(){
+            console.log("div");
+        },
+        clickP(e){
+            // e.stopPropagation();
+            console.log("p");
+        },
+    }
+}
+
+</script>
+
+<template>
+
+    <h3>事件修饰符</h3>
+
+    <a @click.prevent="clickHandle" href="https://baidu.com">示例链接</a>
+
+    <div @click="clickDiv">
+        <p @click.stop="clickP">测试冒泡</p>
+    </div>
+
+</template>
