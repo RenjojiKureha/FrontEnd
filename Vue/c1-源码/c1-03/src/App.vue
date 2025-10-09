d04582e212250de0b70250a0ca329b4b16ddf687
renjoji kureha <nimadezufen@gmail.com>
2025-10-09 18:57:07 +0800

    vue16

---
+++ Vue/c1-源码/c1-03/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+
+// 第一步：引入组件
+import myComponent from "./components/16-myComponent.vue"
+
+export default {
+  // 第二步：注入组件
+  components:{
+    myComponent,
+  }
+}
+
+</script>
+
+<style>
+
+</style>
+
+<template>
+
+  <!-- 第三步：显示组件 -->
+  <myComponent/>
+
+</template>
---
+++ Vue/c1-源码/c1-03/src/components/16-myComponent.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+  data(){
+    return{
+      message:"组件基础组成"
+    }
+  }
+}
+
+</script>
+
+<!-- scpoed：让当前样式只在当前组件生效 -->
+<style scoped>
+
+.container{
+  font-size: 30px;
+  color: red;
+}
+
+</style>
+
+<!-- 此为必须存在，上面两者任意 -->
+<template>
+
+  <div class="container">{{ message }}</div>
+
+</template>
