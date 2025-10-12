709d7010e2044802c818002e0d34beffcc0bf432
renjoji kureha <nimadezufen@gmail.com>
2025-10-12 20:15:29 +0800

    vue28

--- Vue/源码/c3-01/src/App.vue
+++ Vue/源码/c3-01/src/App.vue
@@ -1,30 +1,47 @@
 <template>
+
   <!-- <slotBase>
     <h3>插槽标题</h3>
     <p>插槽内容</p>
   </slotBase> -->
-  <slotsTwo>
+
+  <!-- <slotsTwo>
     <template #header>
       <h3>{{ msg }}</h3>
     </template>
     <template v-slot:main>
       <p>这是内容</p>
     </template>
-  </slotsTwo>
+  </slotsTwo> -->
+
+  <slotsAttr>
+    <template #header="slotProps">
+      <h3>{{ currentTest }}-{{ slotProps.msg }}</h3>
+    </template>
+    <template #main="slotProps">
+      <p>{{ slotProps.job }}</p>
+    </template>
+  </slotsAttr>
+
 </template>
 
 <script>
+
 import slotsBase from './components/slotsBase.vue';
 import slotsTwo from './components/slotsTwo.vue';
+import slotsAttr from './components/slotsAttr.vue';
+
 export default {
   name: 'App',
   components: {
     slotsBase,
-    slotsTwo
+    slotsTwo,
+    slotsAttr,
   },
   data() {
     return {
-      msg: '插槽标题'
+      msg: '插槽标题',
+      currentTest: '测试内容',
     };
   }
 }
---
+++ Vue/源码/c3-01/src/components/slotsAttr.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>再续集</h3>
+    <slot name="header" :msg="childMessage"></slot>
+    <slot name="main" :job="jobMsg"></slot>
+</template>
+
+<script>
+export default {
+    name: 'slotsAttr',
+    data() {
+        return {
+            childMessage: '子组件数据',
+            jobMsg: '前端工程师'
+        };
+    }
+}
+</script>
