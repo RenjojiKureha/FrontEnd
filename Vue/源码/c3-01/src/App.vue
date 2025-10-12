027a113a9f643087829a05574e1f0cd8ca982833
renjoji kureha <nimadezufen@gmail.com>
2025-10-12 17:07:51 +0800

    vue26

---
+++ Vue/源码/c3-01/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <slotBase>
+    <h3>插槽标题</h3>
+    <p>插槽内容</p>
+  </slotBase>
+</template>
+
+<script>
+import slotsBase from './components/slotsBase.vue';
+export default {
+  name: 'App',
+  components: {
+    slotsBase
+  }
+}
+</script>
