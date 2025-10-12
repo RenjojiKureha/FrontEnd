08ff10ebf8d70b5360b08a3e91e339d6c47546c8
renjoji kureha <nimadezufen@gmail.com>
2025-10-12 19:44:43 +0800

    vue27

--- Vue/源码/c3-01/src/App.vue
+++ Vue/源码/c3-01/src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <slotBase>
+  <!-- <slotBase>
     <h3>插槽标题</h3>
     <p>插槽内容</p>
-  </slotBase>
+  </slotBase> -->
+  <slotsTwo>
+    <template #header>
+      <h3>{{ msg }}</h3>
+    </template>
+    <template v-slot:main>
+      <p>这是内容</p>
+    </template>
+  </slotsTwo>
 </template>
 
 <script>
 import slotsBase from './components/slotsBase.vue';
+import slotsTwo from './components/slotsTwo.vue';
 export default {
   name: 'App',
   components: {
-    slotsBase
+    slotsBase,
+    slotsTwo
+  },
+  data() {
+    return {
+      msg: '插槽标题'
+    };
   }
 }
 </script>
---
+++ Vue/源码/c3-01/src/components/slotsTwo.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>slots续集</h3>
+    <slot name="header"></slot>
+    <a href="#">我是插入在中间的内容</a>
+    <slot name="main"></slot>
+</template>
+
+<script>
+export default {
+    name: 'slotsTwo',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
