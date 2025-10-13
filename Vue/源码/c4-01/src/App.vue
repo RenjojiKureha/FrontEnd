755e8784e11c5ac928ccd0819053a4ddff2f338a
renjoji kureha <nimadezufen@gmail.com>
2025-10-13 16:08:26 +0800

    vue30

--- Vue/源码/c4-01/src/App.vue
+++ Vue/源码/c4-01/src/App.vue
@@ -1,53 +1,14 @@
 <template>
-  <h3>组件生命周期</h3>
-  <p>{{ msg }}</p>
-  <button @click="msgHandle">更新数据</button>
+<userComponent/>
 </template>
 
 <script>
-import { mergeAlias } from 'vite';
-
-
-// 生命周期函数
-// 创建期：beforeCreate created
-// 挂载期：beforeMount mounted
-// 更新期：beforeUpdate updated
-// 销毁期：beforeUnmount unmounted
-
+import myComponent from "./components/myComponent.vue"; 
+import userComponent from "./components/userComponent.vue";
 export default {
-  data() {
-    return {
-      msg: "更新之前",
-    };
-  },
-  methods: {
-    msgHandle() {
-      this.msg = "更新之后";
-    },
-  },
-  beforeCreate() {
-    console.log("组件创建之前");
-  },
-  created() {
-    console.log("组件创建之后");
-  },
-  beforeMount() {
-    console.log("组件渲染之前");
-  },
-  mounted() {
-    console.log("组件渲染之后");
-  },
-  beforeUpdate() {
-    console.log("组件更新之前");
-  },
-  updated() {
-    console.log("组件更新之后");
-  },
-  beforeUnmount() {
-    console.log("组件销毁之前");
-  },
-  unmounted() {
-    console.log("组件销毁之后");
-  },
+  components: {
+    myComponent,
+    userComponent
+  }
 };
 </script>
---
+++ Vue/源码/c4-01/src/components/userComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <h3>组件生命周期函数应用</h3>
+    <p ref="name">这是一个文案</p>
+    <ul>
+        <li v-for="(item,index) in banner" :key="index">
+            <h3>{{ item.title }}</h3>
+            <p>{{ item.content }}</p>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            banner:[]
+        };
+    },
+    created() {  // beforeCreate不行
+        this.banner = [
+            { title: "标题1", content: "内容1" },
+            { title: "标题2", content: "内容2" },
+            { title: "标题3", content: "内容3" },
+        ];
+    },
+    beforeMount() {
+        console.log(this.$refs.name); // undefined
+    },
+    mounted() {
+        console.log(this.$refs.name);
+    },
+};
+</script>
