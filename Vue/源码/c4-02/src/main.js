bea99a618f0faf95c1c6d3777919e4b0430b24bc
renjoji kureha <nimadezufen@gmail.com>
2025-10-14 19:54:11 +0800

    vue35

--- Vue/源码/c4-02/src/main.js
+++ Vue/源码/c4-02/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// app:vue的实例对象
+// 在一个vue项目中，有且只有一个vue的实例对象
 const app = createApp(App)
+
+// 挂载到容器当中
 app.mount('#app')
 app.provide('msg','全局数据')
