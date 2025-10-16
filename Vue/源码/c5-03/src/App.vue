84a445b1a9b761b3b23e89ae0f2d19a6ee648167
renjoji kureha <nimadezufen@gmail.com>
2025-10-17 00:07:27 +0800

    vue 48

--- Vue/源码/c5-03/src/App.vue
+++ Vue/源码/c5-03/src/App.vue
@@ -2,15 +2,18 @@
   <directiveScoped/>
   <p v-blue>用全局自定义方法变蓝</p>
   <directiveLife/>
+  <directiveParams/>
 </template>
 
 <script>
 import directiveScoped from './components/directiveScoped.vue';
 import directiveLife from './components/directiveLife.vue';
+import directiveParams from './components/directiveParams.vue';
 export default {
   components:{
     directiveScoped,
     directiveLife,
+    directiveParams
   }
 }
 </script>
---
+++ Vue/源码/c5-03/src/components/directiveParams.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>自定义指令钩子函数的参数</h3>
+    <p v-myShow:foo="flag">{{ msg }}</p>
+    <button @click="updateHandle">切换</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref("模拟v-show指令")
+const flag = ref(true)
+
+function updateHandle(){
+    flag.value = flag.value === true ? flag.value = false : flag.value = true
+}
+
+// 模拟v-show指令
+const vMyShow = {
+    updated(el, binding){
+        binding.value === true ? el.style.display = "block" : el.style.display = "none"
+    }
+}
+</script>
