05ca682bbee4a5699246168b940fa375187de93b
renjoji kureha <nimadezufen@gmail.com>
2025-10-16 23:41:46 +0800

    vue 47

--- Vue/源码/c5-03/src/App.vue
+++ Vue/源码/c5-03/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <directiveScoped/>
   <p v-blue>用全局自定义方法变蓝</p>
+  <directiveLife/>
 </template>
 
 <script>
 import directiveScoped from './components/directiveScoped.vue';
+import directiveLife from './components/directiveLife.vue';
 export default {
   components:{
     directiveScoped,
+    directiveLife,
   }
 }
 </script>
---
+++ Vue/源码/c5-03/src/components/directiveLife.vue
@@ -0,0 +1,54 @@
+<template>
+    <h3>自定义指令的钩子函数</h3>
+    <p v-red v-if="flag">{{ msg }}</p>
+    <button @click="update">修改内容</button>
+    <button @click="del">删除元素</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref("红色效果")
+const flag = ref(true)
+
+function update(){
+    msg.value = "改之"
+}
+
+function del(){
+    flag.value = false
+}
+
+const vRed = {
+    // 每个里头都有这四个
+    created(el, binding, vnode, prevVnode){
+        console.log("created");
+        
+    },
+    beforeMount(){
+        console.log("beforeMount");
+        
+    },
+    mounted(){
+        console.log("mounted");
+        
+    },
+    beforeUpdate(){
+        console.log("beforeUpdate");
+        
+    },
+    updated(){
+        console.log("updated");
+        
+    },
+    beforeUnmount(){
+        console.log("beforeUnmount");
+        
+    },
+    unmounted(){
+        console.log("unmounted");
+        
+    }
+}
+
+</script>
